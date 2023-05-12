a88bfb962fef973fae216b06e9e7c3146d752c3c
Rafael ( RAFILKLDEV ) <rafaelalmeidapenha@gmail.com>
2023-05-11 23:25:33 -0300

    testando build

--- src/components/productMarketing.tsx
+++ src/components/productMarketing.tsx
@@ -9,26 +9,24 @@ import {
 export function ProductMarketing() {
   return (
     <>
-    <ProductMarketingS border={"bot"}>
-      <ProductMarketingBar>
-        <ProductMarketingTitle>Lectos Clinico</ProductMarketingTitle>
-        <ProductMarketingText>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </ProductMarketingText>
-      </ProductMarketingBar>
-      <ProductMarketingImg src="https://blog.portalpos.com.br/app/uploads/2021/03/Menina-jovem-aparentemente-se-consultando-com-uma-psicologa-ou-pedagoga-1024x683.jpg" />
-    </ProductMarketingS>
-    <ProductMarketingS border="bot">
-      
-    </ProductMarketingS>
+      <ProductMarketingS border={"bot"}>
+        <ProductMarketingBar>
+          <ProductMarketingTitle>Lectos Clinico</ProductMarketingTitle>
+          <ProductMarketingText>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </ProductMarketingText>
+        </ProductMarketingBar>
+        <ProductMarketingImg src="https://blog.portalpos.com.br/app/uploads/2021/03/Menina-jovem-aparentemente-se-consultando-com-uma-psicologa-ou-pedagoga-1024x683.jpg" />
+      </ProductMarketingS>
+      <ProductMarketingS border="bot"></ProductMarketingS>
     </>
   );
 }
